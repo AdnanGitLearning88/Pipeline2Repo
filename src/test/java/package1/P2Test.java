f3f7249390657557911eec62a947a9da26332843
Adnan Shaikh <adshaikh@ecitele.com>
2022-01-11 19:45:09 +0530

    First Commit

---
+++ src/test/java/package1/P2Test.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class P2Test {
+
+	@Test
+	public void testP2() throws Exception {
+		
+		System.out.println("Executing Test in Pipeline 2");
+		
+	}
+	
+}
